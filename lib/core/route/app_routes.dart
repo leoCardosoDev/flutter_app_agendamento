bd03f4d0824556ed2e895d5546f5bf12bd4e7e53
leoCardosoDev <leocardosodev@gmail.com>
2023-12-30 18:16:12 -0300

    feat: add route app_routes

---
+++ lib/core/route/app_routes.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+final GoRouter router = GoRouter(
+  initialLocation: '/',
+  debugLogDiagnostics: true,
+  routes: <RouteBase>[
+    GoRoute(
+      path: '/',
+      builder: (BuildContext context, GoRouterState state) {
+        return const Placeholder();
+      }
+    ),
+  ],
+);
