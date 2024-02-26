6b3ad682325e42d81ccfaa5033ba9fb5e8bc0fee
leoCardosoDev <leocardosodev@gmail.com>
2024-02-26 10:16:02 -0300

    feat: add repository

--- lib/features/auth/data/auth_repository.dart
+++ lib/features/auth/data/auth_repository.dart
@@ -3,7 +3,7 @@ import './auth_datasource.dart';
 class AuthRepository {
   final AuthDataSource _dataSource = AuthDataSource();
 
-  Future<void> login(String email, String password) {
-    return _dataSource.login(email, password);
+  Future<void> login({required String email, required String password}) {
+    return _dataSource.login(email: email, password: password);
   }
 }
