9ead67d49cbb1d4c8fa3690eeb306926981d0a8c
leoCardosoDev <leocardosodev@gmail.com>
2024-03-01 08:07:33 -0300

    feat: add generics to respository

--- lib/features/auth/data/auth_repository.dart
+++ lib/features/auth/data/auth_repository.dart
@@ -1,9 +1,11 @@
+import '../models/user.dart';
 import './auth_datasource.dart';
 
 class AuthRepository {
   final AuthDataSource _dataSource = AuthDataSource();
 
-  Future<void> login({required String email, required String password}) {
-    return _dataSource.login(email: email, password: password);
+  Future<Result<LoginFailedResult, User>> login({required String email, required String password}) {
+    final result = _dataSource.login(email: email, password: password);
+    return result;
   }
 }
