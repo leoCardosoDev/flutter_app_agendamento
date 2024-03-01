5656fba3259398cf5a4c7d8e35e4f7e25418ad52
leoCardosoDev <leocardosodev@gmail.com>
2024-03-01 09:12:52 -0300

    refactor: change result return

--- lib/features/auth/data/auth_repository.dart
+++ lib/features/auth/data/auth_repository.dart
@@ -6,8 +6,11 @@ import 'results/login_failed_result.dart';
 class AuthRepository {
   final AuthDataSource _dataSource = AuthDataSource();
 
-  Future<Result<LoginFailedResult, User>> login({required String email, required String password}) {
-    final result = _dataSource.login(email: email, password: password);
+  Future<Result<LoginFailedResult, User>> login({required String email, required String password}) async  {
+    final result = await _dataSource.login(email: email, password: password);
+    if (result case Failure(error: final error)) {
+      print(error);
+    }
     return result;
   }
 }
