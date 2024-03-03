a959afaf6755e97afc307d0a299799cd646cbc68
leoCardosoDev <leocardosodev@gmail.com>
2024-03-03 04:55:59 -0300

    feat: add

--- lib/app.dart
+++ lib/app.dart
@@ -1,14 +1,38 @@
 import 'package:flutter/material.dart';
 
+import 'core/helpers/result.dart';
 import 'core/routes/app_routes.dart';
 import 'core/flavors/flavor_config.dart';
+import 'features/auth/data/auth_repository.dart';
 
 void bootstrap(FlavorConfig config) {
   runApp(const App());
 }
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  void initState() {
+    super.initState();
+    initialize();
+  }
+
+  Future<void> initialize() async {
+    final AuthRepository authRepository = AuthRepository();
+    final result = await authRepository.login(email: 'leosilva@gmail.com', password: '1234');
+    switch(result) {
+      case Success(object: final user):
+        print('Success ${user.id}');
+      case Failure(error: final error):
+        print(error);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
--- lib/features/auth/data/auth_datasource.dart
+++ lib/features/auth/data/auth_datasource.dart
@@ -1,4 +1,5 @@
 import 'package:dio/dio.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
 
 import '../../../core/helpers/result.dart';
 import '../models/user.dart';
@@ -13,14 +14,17 @@ class AuthDataSource {
         'X-Parse-REST-API-Key': 'hewuQrYiZfKHwWhUHS1XmUz3hWme0NPI9tjkt0Rh'
       }
     )
-  );
+  )..interceptors.add(PrettyDioLogger(requestHeader: true, requestBody: true));
 
   Future<Result<LoginFailedResult, User>> login({required String email, required String password}) async {
     try {
       final response = await _dio.post('/v1-sign-in', data: { 'email': email, 'password': password });
       return Success(User.fromMap(response.data['result']));
     } on DioException catch (e) {
-      if (e.response?.statusCode == 401) {
+      if (e.type == DioExceptionType.connectionError) {
+        return const Failure(LoginFailedResult.offline);
+      }
+      else if (e.response?.statusCode == 404) {
         return const Failure(LoginFailedResult.invalidCredentials);
       }
       return const Failure(LoginFailedResult.unknownError);
